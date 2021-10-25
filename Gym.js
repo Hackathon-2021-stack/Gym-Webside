91f6ff07779e6702ecc3b66823934d14fb3144ca
Aditya Banik <adityabanik19@gmail.com>
2021-10-25 21:50:16 +0530

    25-10-2021

---
+++ Gym.js
@@ -0,0 +1,10 @@
+let total = 0
+let l = prompt("what is your Number?");
+
+
+for (let I = 0; I <= +l; I++) {
+    let total = I+I
+    let t = total+total
+    console.log(I+"+"+I+"+"+total+" = "+t)
+    
+}
